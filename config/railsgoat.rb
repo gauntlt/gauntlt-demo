ae143c5abf48c5ad18764d7cf05d66d112462b15
James Wickett <wickett@gmail.com>
2013-10-29 11:48:16 -0500

    broke out railsgoat into its own service file so that we could start separately

---
+++ config/railsgoat.rb
@@ -0,0 +1,16 @@
+require 'service_manager'
+require 'bundler'
+
+Bundler.with_clean_env do
+  Dir.chdir "./vendor/railsgoat" do 
+    `bundle install --binstubs`
+  end
+  
+  ServiceManager.define_service "railsgoat" do |s|
+    s.start_cmd = "rake db:setup && rails s -p 3000"
+    s.loaded_cue = /WEBrick::HTTPServer#start/
+    s.cwd = Dir.pwd + "/vendor/railsgoat/"
+    s.pid_file = 'railsgoat.pid'
+  end
+   
+end
