e10a9f93e6d3fc22b391dd3f6f212fb7e5701ea3
hacker <hacker@localhost.localdomain>
2015-12-09 00:40:51 +0100

    Working authenticaiton

--- examples/webgoat/vuln-05/attack05.py
+++ examples/webgoat/vuln-05/attack05.py
@@ -1,42 +1,29 @@
-#!/bin/bash
+import requests
+import pprint
 
-## Pretend this is a working Bash script - for now, this is just a shell
-## For my money, I'd actually write this in Python or Ruby
-## So consider this pseudo-code for now
 
-# --[ STEP 00 ]--
-# Request login page
 
-# --[ STEP 01 ]--
-# Log into WebGoat
+url = 'http://127.0.0.1:8080/WebGoat'
+values = {'username': 'guest',
+        'password': 'guest'}
 
-# --[ STEP 02 ]--
+#r = requests.post(url + '/j_spring_security_check' , values)
+
+
+with requests.Session() as s:
+    # Request login page
+    #Log into WebGoat
+
+    r = s.post(url+'/j_spring_security_check', values)
+    print (r.content)
+
 # Figure out which menu item is "Http Basics"
 
-# --[ STEP 3 ]--
 # Request the lesson for General => Http Basics
 
-# --[ STEP 4 ]--
 # Submit the attack to the General => Http Basics page
-#ATTACK=`echo -n "1"`
 
 # Purposefully fail for testing purposes
-#ATTACK=`echo -n "1"`
 
-# --[ STEP 6 ]--
-# Set the correct exit code
-# It will return a
-# - 0 (error) if the vulnerability is present
-# - 1 (success) if the vulnerability is fixed (aka not present)
-
-#if [ $ATTACK -eq 1 ]
-#then
-#    # Attack successful
-#    echo "Attack Successful"
-#    exit 1
-#else
-#    # Attack failed - no vuln-00 present
-#    echo "vuln-00 not present"
-#    exit 0
-#f
-print("Attack Successfull")
+#if successfull, say so otherwise don't #logic
+pprint.pprint("Attack Successfull")
