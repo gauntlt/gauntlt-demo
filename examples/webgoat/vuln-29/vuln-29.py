f149bda43d5112a29542f454d43aa43c2e42613a
Juan Villegas <jjv@wireless-10-148-134-17.public.utexas.edu>
2015-12-11 15:08:21 -0600

    Vuln-29 is ready

--- examples/webgoat/vuln-29/vuln-29.py
+++ examples/webgoat/vuln-29/vuln-29.py
@@ -8,7 +8,6 @@ login_credentials={
 	'username': 'guest',
 	'password': 'guest'
 }
-
 attack_fields={
 	'gId': 'Gmail id',
 	'gPass': 'password',
@@ -19,16 +18,15 @@ attack_fields={
 }
 
 current_session = requests.session()
-
 home= current_session.get("http://127.0.0.1:8080/WebGoat/login.mvc")
 login= current_session.post('http://127.0.0.1:8080/WebGoat/j_spring_security_check',login_credentials)
-
 login= current_session.get("http://127.0.0.1:8080/WebGoat/service/lessonmenu.mvc")
+
 our_site_link = json.loads(login.text)[16].get('children')[2].get('link')
+vuln29_menu_page="http://127.0.0.1:8080/WebGoat/"+our_site_link
 
-attack_page= current_session.get("http://127.0.0.1:8080/WebGoat/"+our_site_link)
-
-attack_page= current_session.post("http://127.0.0.1:8080/WebGoat/"+our_site_link, attack_fields)
+attack_page= current_session.get(vuln29_menu_page)
+attack_page= current_session.post(vuln29_menu_page, attack_fields)
 
 if attack_page.text.find('Congratulations. You have successfully completed this lesson'):
   print "Vulnerability Found"
